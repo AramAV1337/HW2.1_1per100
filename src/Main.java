2237b3677c13cf4ff4c5cc284a28968f11d27cc4
Aram Avetisov <aram.avetisov@inbox.ru>
2021-05-14 13:14:33 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int accAmount = 100; // Сумма на счету
+        int enominator = 100; // Делитель для рассчета бонуса
+        int bonus;
+        int less = 200; // С чем сравниваем
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите сумму пополнения в рублях: ");
+        int refill = in.nextInt();
+        int accAmountTarget = refill + accAmount;
+        in.close();
+        bonus = refill / enominator;
+        if (accAmountTarget <= less)
+        {
+            System.out.println("Ваш счет равен: " +accAmountTarget);
+            System.out.println("Бонус не начисляется");
+        }
+        else
+        {
+            System.out.println("Ваш счет в рублях равен: " +(bonus + (refill+ accAmount)));
+            System.out.println("Из них бонусные рубли: " +bonus);
+        }
+
+    }
+}
